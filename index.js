ff71d5d69e523c05e487a78da588566b114ca659
nikiman8888 <nikiman88@yahoo.com>
2020-07-29 15:53:58 +0300

    Working solution,need refactoring

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const prompt = require("prompt-sync")({ sigint: true }); // // with propmp we can read user input in the terminal
-const printMatrix = require("./printMatrix");
-const counter = require("./countGreenNeighbours");
-const fillGridClone = require("./fillGridClone");
-const  rowNumbersValidation = require('./inputValidations');
+const prompt = require("prompt-sync")({ sigint: true }); // // with prompt we can read user input in the terminal
+const printMatrix = require("./printMatrix");   // return grid (matrix)
+const counter = require("./countGreenNeighbours"); // count green neighbour cells of given cell
+const fillGridClone = require("./fillGridClone"); // fill the grid clone 
+const  validations = require('./inputValidations'); // validate inputs from terminal
 
 function startGame() {
+   //--------------------- Accept grid size arguments and validate --------------------
     let width,height; 
     let gridSize = prompt("Enter width and height of the grid:  ");  // get the user input grid size
     let splited = gridSize.split(", ");
@@ -22,65 +23,95 @@ function startGame() {
             break;
         }
     }
-  } // dotuk e gotovo 
-  
-  let grid = new Array(height); // create the game grid rows       // moje za classa da se iznese
-  //let searchedRow = coordinates[1];
-  //let searchedCol = coordinates[0];
-
+  } 
+  // ----------------------------Accept and create the Generation Zero state of the grid and validate the inputs -------
+  let grid = new Array(height); // create the game grid rows    
+ 
   for (let row = 0; row < grid.length; row++) {    // intialize our Generation zero state of the grid
     //grid[row] = Math.random().toString(2).substr(2, width);
     while(true){
         let currentGridRow = prompt(`Enter a ${row+1} grid row. Numbers should be created with 0-1 with ${width} length:  `);
         if(currentGridRow.length === width){
-            if(rowNumbersValidation(currentGridRow)){
+            if(validations.rowNumbersValidation(currentGridRow)){
                 grid[row] = currentGridRow; //triabva i proverka dali za corektni 4isla
             break;
             }else {
-                console.log('Wrong width or numbers are not 0-1')
+                console.log('Wrong length or numbers are not 0-1')
             }
             
         }else{
-            console.log('Wrong width or numbers are not 0-1')
+            console.log('Wrong length or numbers are not 0-1')
         }
     }
-    // get user input for the row //0010
   }
-  console.log(printMatrix(grid));
-  return;
-// DOOTUK TRIABVA DA E VIARNO
+    //-------------Accept the last arguments (coordinates and generation count) and validate inputs -----------------
 
-  let poitGreenCounter = 0;
-  for (let index = 0; index < generation; index++) {
+    let arguments = prompt("Enter the x1 and y1 (column,row) of the point and generationCount wth comma and space: ")
+    let splitedArgs = arguments.split(", ");
+    let x1Col,y1Row,generations;
+
+    while(true){  
+                   
+      if (splitedArgs.length !== 3){
+        
+        arguments = prompt("Wrong! Enter correct x1 and y1 (column,row) of the point and generationCount wth comma and space:  ...(3, 3, 9): ");
+        splitedArgs = arguments.split(", ");
+        console.log('start')
+      }else {
+        
+        x1Col = splitedArgs[0];
+        y1Row = splitedArgs[1];
+        generations = splitedArgs[2];
+        
+        console.log(validations.isCoordinatesInGrid(grid,x1Col,y1Row));
+        if(validations.isNumber(x1Col) &&   //if all inputs are real numbers and coordPoits is inside the grid
+           validations.isNumber(y1Row) &&
+           validations.isNumber(generations) &&
+           validations.isCoordinatesInGrid(grid,x1Col,y1Row)){
+             console.log('corect')
+            break;
+        }else{   
+          if(validations.isCoordinatesInGrid(grid,x1Col,y1Row))   {
+            arguments = prompt("Coordinates are not inside the Grid.Enter correct coordinates and generationCount:  ")
+          }  else {
+            arguments = prompt(" Enter real numbers x1 and y1 (column,row) of the point and generationCount wth comma and space -->  ...(3, 3, 9): ");
+          }                                     
+          splitedArgs = arguments.split(", ");
+        }
+      }
+    }
+
+    //return;
+  let targetPointRow = y1Row;
+  let targetPointCol = x1Col;
+  let targetPointGreenCounter = 0;
+  for (let index = 0; index < generations; index++) {
     // start iterate generations
-    let cloneMatrix = new Array(3) //height
+    let cloneMatrix = new Array(height) //height
       .fill(null)
-      .map(() => new Array(3).fill(null)); // create empty clone grid to fill it with next generation values
+      .map(() => new Array(width).fill(null)); // create empty clone grid to fill it with next generation values
 
     for (let row = 0; row < grid.length; row++) {
       for (let col = 0; col < grid[row].length; col++) {
         let greenNeighbours = counter(grid, row, col).toString();
         let currentCell = grid[row][col];
-        cloneMatrix = fillGridClone(
+        cloneMatrix = fillGridClone(  // we fill cell by cell the empty clone grid
           currentCell,
           cloneMatrix,
           greenNeighbours,
           row,
           col
-        ); // here we fill each cell of the cloned grid
+        ); 
       }
     }
 
-    grid = cloneMatrix;
+    grid = cloneMatrix; // old grid state get the new generation state from the cloned grid
     console.log(printMatrix(grid));
-    if (grid[searchedRow][searchedCol] === "1") {
-      poitGreenCounter++;
+    if (grid[targetPointRow][targetPointCol] === "1") { // count the targeted point is  green in the current generation
+      targetPointGreenCounter++;
     }
   }
-  console.log(poitGreenCounter);
-
-  //console.log( printMatrix(cloneMatrix));
-  //console.log(typeof(printMatrix(cloneMatrix)));
+  console.log(`The target point with coordinates [${targetPointCol},${targetPointRow}](column,row) become green ${poitGreenCounter} times for ${generations} generations`);
+ 
 }
-
 startGame();
--- inputValidations.js
+++ inputValidations.js
@@ -1,4 +1,4 @@
-function rowNumbersValidation (numbers) {  // check is aour row of grid is created from 0-1 digits
+function rowNumbersValidation (numbers) {  // check is our row of grid is created from 0-1 digits
     let arr = ['0','1']
     for (let index = 0; index < numbers.length; index++) {
         let currentDigit = numbers[index];
@@ -8,5 +8,23 @@ function rowNumbersValidation (numbers) {  // check is aour row of grid is creat
     }
     return true;
 }
-module.exports = 
-    rowNumbersValidation;
+
+function isCoordinatesInGrid(grid,column,row) { //check for correct insfe grid coordinates
+    
+    let rowCondition  = row >=0 && row <= grid.length-1? true:false; // check rowPoint is inside the gridRows
+    let columnCondition = column >=0 && column <= grid[0].length-1 ? true:false; //check columnPoint is inside the gridColumns
+
+    if(rowCondition && columnCondition) {
+        return true;
+    }
+    return false;
+}
+function isNumber (number) { 
+    //check is number real
+    if(isNaN(number)){
+        return false;
+    }
+    return true;
+}
+module.exports = { rowNumbersValidation,isCoordinatesInGrid, isNumber }
+  
